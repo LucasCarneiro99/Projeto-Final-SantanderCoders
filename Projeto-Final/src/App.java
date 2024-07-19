b5a9b78e87dfd285e33a266f7657f9e1903344b2
LucasCarneiro99 <lucasbcarneiro903@gmail.com>
2024-07-19 19:50:46 -0300

    alteracoes finais

--- Projeto-Final/src/App.java
+++ Projeto-Final/src/App.java
@@ -31,25 +31,25 @@ public class App {
         } else if (salarioBruto <= 2427.35) {
             inss = salarioBruto * 0.09;
         } else if (salarioBruto <= 3641.03) {
-            inss =salarioBruto * 0.12;
+            inss = salarioBruto * 0.12;
         } else {
             inss = salarioBruto * 0.14;
         }
         return inss;
     }
     
-    public static double calcularImpostoDeRenda(double salarioBase) {
+    public static double calcularImpostoDeRenda(double salarioBruto) {
         double impostoDeRenda = 0.0;
-        if (salarioBase <= 1903.98) {
+        if (salarioBruto <= 1903.98) {
             impostoDeRenda = 0.0;
-        } else if (salarioBase <= 2826.65) {
-            impostoDeRenda = salarioBase*0.075;
-        } else if (salarioBase <= 3751.05) {
-            impostoDeRenda = salarioBase * 0.15;
-        } else if (salarioBase <= 4664.68) {
-            impostoDeRenda = salarioBase * 0.225;
+        } else if (salarioBruto <= 2826.65) {
+            impostoDeRenda = salarioBruto*0.075;
+        } else if (salarioBruto <= 3751.05) {
+            impostoDeRenda = salarioBruto * 0.15;
+        } else if (salarioBruto <= 4664.68) {
+            impostoDeRenda = salarioBruto * 0.225;
         } else {
-            impostoDeRenda = salarioBase * 0.275;
+            impostoDeRenda = salarioBruto * 0.275;
         }
         return impostoDeRenda;
     }
